443d71d6899c47fa4efc50ebf4fe72ae0e742ce9
nikk7007 <nik700789@gmail.com>
2025-10-11 11:46:49 -0300

    cadastro php

--- config/funcs.php
+++ config/funcs.php
@@ -13,4 +13,39 @@ function mostrarHora($hora) {
   return implode(":", $hora);
 }
 
+function limpar_texto($str){ 
+  return preg_replace("/[^0-9]/", "", $str); 
+}
+
+function verify($email, $senha, $nome = "Nome", $telefone = "19912341234") {
+  $infos = [
+    "email" => true,
+    "senha" => true,
+    "nome" => true,
+    "telefone" => true
+  ];
+  // Verificar email
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $infos["email"] = false;
+  }
+  
+  // Verificar senha
+  if (strlen($senha) < 6 || strlen($senha) > 16) {
+    $infos["senha"] = false;
+  }
+
+  // Verificar nome
+  if (strlen($nome) < 2 || strlen($nome) > 100) {
+    $infos["nome"] = false;
+  }
+
+  // Verificar telefone
+  $telefone = limpar_texto($telefone);
+  if (strlen($telefone) != 11 && strlen($telefone) != 0) {
+    $infos["telefone"] = false;
+  }
+
+  return $infos;
+}
+
 ?>
