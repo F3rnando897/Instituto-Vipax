8146a1cf2a7cc0c0cee5bdc31ab2f2f3f4120147
Scaboli <scabolij@gmail.com>
2025-11-08 13:17:57 -0300

    dashboard

---
+++ dashboard/painel_de_controle/painel_controle.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel de controle</title>
+    <link rel="stylesheet" href="../dashBoard.css">
+    <link rel="stylesheet" href="painel.css">
+</head>
+<body>
+   <div class="sidebar">
+        <div class="menu-icon">☰</div>
+    </div>
+
+    <div class="main-content">
+        <h1>Painel de controle</h1>
+
+        <div class="cards-container">
+            <div class="card">
+                <h2>Objetivos</h2>
+                <p><b>x objetivos salvos</b></p>
+            </div>
+
+            <div class="card">
+                <h2>Galeria</h2>
+                <p><b>x fotos salvas</b></p>
+            </div>
+
+            <div class="card">
+                <h2>Eventos comuns</h2>
+                <p><b>x eventos existente</b></p>
+            </div>
+
+            <div class="card">
+                <h2>Eventos marcados</h2>
+                <p><b>x eventos marcados</b></p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
