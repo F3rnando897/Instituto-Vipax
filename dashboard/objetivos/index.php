ad9469082a664448514da7c74f4a8ed1cb2332d8
F3rnando897 <fernandojesusmatos123@gmail.com>
2025-11-08 13:12:23 -0300

    dashboard

--- dashboard/objetivos/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include "/criar_evento.php";
-    include "/editar_evento.php";
-    include "/excluir_evento.php";
-    
-?>
---
+++ dashboard/objetivos/objetivos.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="objetivos.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+    <title>Dashboard</title>
+</head>
+<body id="dash">
+    <div class="dashboard">
+        <div class="dashboard-wrap">
+            <aside class="sidebar">
+                <nav class="menu-items">
+                    <i class="fa-solid fa-bars"></i>
+                    
+                </nav>
+            </aside>
+        </div>
+    
+    <main>
+        <h1>Objetivos</h1>
+        <div class="objetivo-card">
+            <h3>Valores</h3>
+            <p>Valorizamos a escuta circular do silêncio pleno, permitindo que o não-dito também tenha voz nas tomadas de decisão plurais. Acreditamos que o autoconhecimento é o combustível etéreo que move a engrenagem do impacto planetário sustentável. Assim, buscamos constantemente o equilíbrio entre a performance energética e a entrega de valor intangível, garantindo que cada KPI seja um reflexo cristalino da jornada interior. Em resumo: nosso valor maior é transformar a experiência humana em um laboratório de possibilidades infinitas, onde cada insight é uma estrela a mais no céu corporativo.</p>
+            <div class="buttons">
+            <span class="delete"><a href="">Delete</a></span>
+            <span class="salvar"><a href="">Salvar</a></span>
+            </div>
+        </div>
+
+        <div class="criar-objetivo">
+            <h3>Criar Novo</h3>
+            <span><i class="fa-solid fa-plus"></i></span> 
+        </div>
+        <div>
+            <span><a href="">Salvar</a></span>
+        </div>
+    </main>
+</body>
+</html>
